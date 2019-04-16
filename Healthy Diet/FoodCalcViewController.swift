98b53ccce3b5b360d5bd1447b36ffe39760b0407
Ryan Lokugamage <rlokugamage@gmail.com>
2019-04-16 08:54:31 -0500

    added FoodCalcViewController and changed item 1 to Food Calc

---
+++ Healthy Diet/FoodCalcViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FoodCalcViewController.swift
+//  Healthy Diet
+//
+//  Created by Ryan Lokugamage on 4/16/19.
+//  Copyright © 2019 Tommy Ertl. All rights reserved.
+//
+
+import UIKit
+
+class FoodCalcViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
