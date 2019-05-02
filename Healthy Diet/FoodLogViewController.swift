257d46ea04c0529e1ece830b2bb4fd89002afc53
Tommy <tregf7@mail.missouri.edu>
2019-05-02 12:49:35 -0500

    Added date picker and container with table view

---
+++ Healthy Diet/FoodLogViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FoodLogViewController.swift
+//  Healthy Diet
+//
+//  Created by Tommy Ertl on 5/2/19.
+//  Copyright © 2019 Tommy Ertl. All rights reserved.
+//
+
+import UIKit
+
+class FoodLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
