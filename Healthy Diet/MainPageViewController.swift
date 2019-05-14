c7e2593c2d6dbe84bc34c889cd2df68857aa8fba
Tommy <tregf7@mail.missouri.edu>
2019-05-13 19:04:31 -0500

    Reverted MainPageViewController

--- Healthy Diet/MainPageViewController.swift
+++ Healthy Diet/MainPageViewController.swift
@@ -7,23 +7,10 @@
 //
 
 import UIKit
-import Charts
 
-class Food {
-    var foodName: String?
-    var calorie: Double?
-    
-    init(_ foodName: String , _ calorie: Double) {
-        self.calorie = calorie
-        self.foodName = foodName
-    }
-}
 
 class MainPageViewController: UIViewController {
 
-    @IBOutlet weak var chartView: BarChartView!
-    
-    var foods = [Food("Garlic", 149.0), Food("Yam", 118.0), Food("Banana", 89.0)]
     
     var person: Person?
     
@@ -33,16 +20,4 @@ class MainPageViewController: UIViewController {
         
     }
     
-    func barChartUpdate() {
-        let entry1 = BarChartDataEntry(x: 1.0, y: 149.0)
-        let entry2 = BarChartDataEntry(x: 2.0, y: 118.0)
-        let entry3 = BarChartDataEntry(x: 3.0, y: 89.0)
-        let dataSet = BarChartDataSet(entries: [entry1, entry2, entry3], label: "foods")
-        let chartData = BarChartData()
-        chartData.addDataSet(dataSet)
-        chartView.data = chartData
-        chartView.chartDescription?.text = "The foods calorie"
-        
-        //chartView.notifyDataSetChanged()
-    }
 }
